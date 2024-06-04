7c49a7fca22b1ab0b5efbb83fc7790cd247b1f6e
lili <lilikazine@gmail.com>
2024-06-04 17:09:40 +0800

    Initial Commit

---
+++ ImageKit/ImageKit.h
@@ -0,0 +1,18 @@
+//
+//  ImageKit.h
+//  ImageKit
+//
+//  Created by LiLi on 2024/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageKit.
+FOUNDATION_EXPORT double ImageKitVersionNumber;
+
+//! Project version string for ImageKit.
+FOUNDATION_EXPORT const unsigned char ImageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageKit/PublicHeader.h>
+
+
