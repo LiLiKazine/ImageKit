9cb9e41dfdef09fc626c53c76e97d142617af4b0
lili <lilikazine@gmail.com>
2024-08-08 11:07:37 +0800

    change dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/LiLiKazine/ArchiverKit.git", branch: "main"),
+        .package(url: "https://github.com/LiLiKazine/ArchiverKit.git", from: Version(0, 1, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
